03b252fb5f6a1d5c660d14055aacbb38ed426fdb
Hardik Jumnani <hardik.jumnani123@gmail.com>
2025-02-16 11:22:27 +0530

    command recorded when speaker says 'hey mirror'

--- main/main.py
+++ main/main.py
@@ -1,10 +1,33 @@
 import cv2
+import speech_recognition as sr # pip install SpeechRecognition
+import threading
+
+listener = sr.Recognizer()
 
 # Type annotation shorthands
 _camera_frame = cv2.typing.MatLike
 
 # Variables
 camera_open: bool = True
+
+def take_command() -> str:
+    command: str = ""
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if command.startswith("hey mirror"):
+                command = command.replace("hey mirror", '')
+                return command
+
+    except Exception as e:
+        print(e)
+
+def process_speech() -> None:
+    command = take_command()
+    if command: print(command)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)  # Open default camera (0)
@@ -20,6 +43,8 @@ if __name__ == "__main__":
         if not ret:
             print("Error: Could not read frame.")
             break
+
+        if threading.active_count() == 1: threading.Thread(target=process_speech, daemon=True).start()
         
         cv2.imshow("Camera Feed", frame)  # Display the frame
         
