f9fd24c0a06b88933d231fee932fb3b8dfa1c71b
NEERASA VEDA VARSHIT <n.vedvarshit@gmail.com>
2025-04-06 12:46:49 +0530

    Update speech_recognition_module.py

--- Final/speech_recognition_module.py
+++ Final/speech_recognition_module.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 
 class SpeechRecognizer:
-    def recognize_speech(self, mic_index=None):
+    def recognize_speech(self, mic_index=3):
         recognizer = sr.Recognizer()
         with sr.Microphone(device_index=mic_index) as source:
             recognizer.adjust_for_ambient_noise(source, duration=1)  # Reduce background noise
